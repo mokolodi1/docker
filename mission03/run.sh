0f35126aa8b004a1a6d9fae68f89430f74b2939b
Teo Fleming <mokolodi1@gmail.com>
2017-03-09 18:03:19 -0800

    open shared folder for mission03

--- mission03/run.sh
+++ mission03/run.sh
@@ -6,5 +6,8 @@ rm -rf /Users/Shared/docker_volume/*
 # link the docker_volume folder to a folder here for ease of access
 ln -s /Users/Shared/docker_volume shared
 
+# open finder so they can see it's for realzies
+open shared
+
 # then mount it with docker...
 docker run -it -v /Users/Shared/docker_volume:/shared ubuntu /bin/bash
